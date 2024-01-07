b7415f21a2495ebf6ffe9beb47daa3db9f912dfd
Anderson <105822681+AndersonFer03@users.noreply.github.com>
2024-01-07 01:16:11 -0600

    feat: game options with X and circle

--- TicTacToe/TicTacToe/game.cpp
+++ TicTacToe/TicTacToe/game.cpp
@@ -1,6 +1,33 @@
 #include "game.h"
 Game::Game() {
 	gameWindow.create(VideoMode(1000, 800), "TicTacToe");
+	playerNumber = 2;
+	paused = false;
+	for (int i = 0;i < ROWS;i++) {
+		for (int j = 0;j < COLUMNS;j++) {
+			matrix[i][j] = 0;
+		}
+	}
+}
+void Game::fillBoard() {
+	board[0][0].x = 220;
+	board[0][0].y = 260;
+	board[0][1].x = 440;
+	board[0][1].y = 260;
+	board[0][2].x = 645;
+	board[0][2].y = 260;
+	board[1][0].x = 220;
+	board[1][0].y = 445;
+	board[1][1].x = 440;
+	board[1][1].y = 445;
+	board[1][2].x = 645;
+	board[1][2].y = 445;
+	board[2][0].x = 220;
+	board[2][0].y = 630;
+	board[2][1].x = 440;
+	board[2][1].y = 630;
+	board[2][2].x = 645;
+	board[2][2].y = 630;
 }
 void Game::render() {
 	gameWindow.clear(Color::White);
@@ -25,36 +52,189 @@ void Game::openGame() {
 	showWindow();
 	play();
 }
-void Game::drawX() {
-	Vector2i mousePosition = Mouse::getPosition(gameWindow);
-	//Texture nextX = Texture();
-	//nextX.loadFromFile("images/X.png");
-	//Sprite spriteX = Sprite();
-	//spriteX.setTexture(nextX);
-	//spriteX.setPosition(200,300);
-	//spriteX.setScale(50, 50);
-	//gameWindow.draw(spriteX);
-	sf::RectangleShape rectangle1(sf::Vector2f(200, 20));
-	rectangle1.setPosition(180, 180);
-	rectangle1.setFillColor(sf::Color::Blue);
-	rectangle1.rotate(45);
+void Game::changeShift() {
+	if (playerNumber == 1) {
+		this->playerNumber = 2;
+	}
+	else {
+		this->playerNumber = 1;
+	}
+}
+void Game::findPlayerNumber(float coordX, float coordY) {
+	if (playerNumber == 1) {
+		drawX(coordX, coordY);
+	}
+	else {
+		drawCircle(coordX, coordY);
+	}
+}
+void Game::findPosition(int row, int column) {
+	if (playerNumber == 1) {
+		matrix[row][column] = 1;
+	}
+	else {
+		matrix[row][column] = 2;
+	}
+}
+void Game::insertTopLeft(Vector2i& mousePosition) {
+	bool isContentY= mousePosition.y >= 260 && mousePosition.y <= 425;
+	bool isContentX = mousePosition.x >= 220 && mousePosition.x<=370;
+	bool isSelected = matrix[0][0] != 0;
+	if (isContentX&&isContentY&&!isSelected) {
+		changeShift();
+		findPlayerNumber(220,260);
+		findPosition(0, 0);
+	}
+}
+void Game::insertTopCentral(Vector2i& mousePosition) {
+	bool isContentY = mousePosition.y >= 260 && mousePosition.y <= 425;
+	bool isContentX = mousePosition.x >= 395 && mousePosition.x <= 600;
+	bool isSelected = matrix[0][1] != 0;
+	if (isContentX && isContentY&&!isSelected) {
+		changeShift();
+		findPlayerNumber(440, 260);
+		findPosition(0, 1);
+	}
+}
 
-	// Crear el segundo rectángulo (diagonal invertida de la X)
-	sf::RectangleShape rectangle2(sf::Vector2f(200, 20));
-	rectangle2.setPosition(165, 305);
-	rectangle2.setFillColor(sf::Color::Blue);
-	rectangle2.rotate(-43);
-	render();
-	gameWindow.draw(rectangle1);
-	gameWindow.draw(rectangle2);
+void Game::insertTopRight(Vector2i& mousePosition){
+	bool isContentY = mousePosition.y >= 260 && mousePosition.y <= 425;
+	bool isContentX = mousePosition.x >= 620 && mousePosition.x <= 785;
+	bool isSelected = matrix[0][2] != 0;
+	if (isContentX && isContentY && !isSelected) {
+		changeShift();
+		findPlayerNumber(645, 260);
+		findPosition(0, 2);
+	}
+}
+void Game::insertCenterRight(Vector2i& mousePosition) {
+	bool isContentY = mousePosition.y >= 440 && mousePosition.y <= 600;
+	bool isContentX = mousePosition.x >= 620 && mousePosition.x <= 785;
+	bool isSelected = matrix[1][2] != 0;
+	if (isContentX && isContentY && !isSelected) {
+		changeShift();
+		findPlayerNumber(645, 445);
+		findPosition(1, 2);
+	}
+}
+void Game::insertCenter(Vector2i& mousePosition) {
+	bool isContentY = mousePosition.y >= 440 && mousePosition.y <= 600;
+	bool isContentX = mousePosition.x >= 395 && mousePosition.x <= 600;
+	bool isSelected = matrix[1][1] != 0;
+	if (isContentX && isContentY && !isSelected) {
+		changeShift();
+		findPlayerNumber(440, 445);
+		findPosition(1, 1);
+	}
+}
+void Game::insertCenterLeft(Vector2i& mousePosition) {
+	bool isContentY = mousePosition.y >= 440 && mousePosition.y <= 600;
+	bool isContentX = mousePosition.x >= 220 && mousePosition.x <= 370;
+	bool isSelected = matrix[1][0] != 0;
+	if (isContentX && isContentY && !isSelected) {
+		changeShift();
+		findPlayerNumber(220, 445);
+		findPosition(1, 0);
+	}
+}
+void Game::insertDownRight(Vector2i& mousePosition) {
+	bool isContentY = mousePosition.y >= 620 && mousePosition.y <= 775;
+	bool isContentX = mousePosition.x >= 620 && mousePosition.x <= 785;
+	bool isSelected = matrix[2][2] != 0;
+	if (isContentX && isContentY && !isSelected) {
+		changeShift();
+		findPlayerNumber(645, 630);
+		findPosition(2, 2);
+	}
+}
+void Game::insertDownCentral(Vector2i& mousePosition) {
+	bool isContentY = mousePosition.y >= 620 && mousePosition.y <= 775;
+	bool isContentX = mousePosition.x >= 395 && mousePosition.x <= 600;
+	bool isSelected = matrix[2][1] != 0;
+	if (isContentX && isContentY && !isSelected) {
+		changeShift();
+		findPlayerNumber(440, 630);
+		findPosition(2, 1);
+	}
+}
+void Game::insertDownLeft(Vector2i& mousePosition) {
+	bool isContentY = mousePosition.y >= 620 && mousePosition.y <= 775;
+	bool isContentX = mousePosition.x >= 220 && mousePosition.x <= 370;
+	bool isSelected = matrix[2][0] != 0;
+	if (isContentX && isContentY && !isSelected) {
+		changeShift();
+		findPlayerNumber(220, 630);
+		findPosition(2, 0);
+	}
+}
+void Game::findCoordinates() {
+	Vector2i mousePosition = Mouse::getPosition(gameWindow);
+	//std::cout << "X:" << mousePosition.x << std::endl;
+	//std::cout << "Y:" << mousePosition.y << std::endl;
+	paused = true;
+	insertTopRight(mousePosition);
+	insertTopCentral(mousePosition);
+	insertTopLeft(mousePosition);
+	insertCenterRight(mousePosition);
+	insertCenter(mousePosition);
+	insertCenterLeft(mousePosition);
+	insertDownRight(mousePosition);
+	insertDownCentral(mousePosition);
+	insertDownLeft(mousePosition);
+
+}
+void Game::reviewBoard() {
+	for (int i = 0;i < ROWS;i++) {
+		for (int j = 0;j < COLUMNS;j++) {
+			refreshBoard(matrix[i][j], i, j);
+		}
+	}
+}
+void Game::refreshBoard(int numPlayer, int row, int column) {
+	bool isSelectedX = numPlayer == 1;
+	bool isSelectedO = numPlayer == 2;
+	if (isSelectedX) {
+		drawX(board[row][column].x, board[row][column].y);
+	}
+	if (isSelectedO) {
+		drawCircle(board[row][column].x, board[row][column].y);
+	}
+}
+void Game::drawCircle(float coordX, float coordY) {
+	Texture nextCircle = Texture();
+	nextCircle.loadFromFile("images/O.png");
+	Sprite spriteCircle = Sprite();
+	spriteCircle.setTexture(nextCircle);
+	spriteCircle.setPosition(coordX-10, coordY);
+	spriteCircle.setScale(0.075f, 0.075f);
+	gameWindow.draw(spriteCircle);
+	//render();
+	//reviewBoard();
+
+	showWindow();
+}
+void Game::drawX(float coordX, float coordY) {
+	Texture nextX = Texture();
+	nextX.loadFromFile("images/X.png");
+	Sprite spriteX = Sprite();
+	spriteX.setTexture(nextX);
+	spriteX.setPosition(coordX,coordY);
+	spriteX.setScale(0.38f, 0.38f);
+	gameWindow.draw(spriteX);
+	//render();
 	showWindow();
 
 }
 void Game::play() {
+	fillBoard();
 	while (gameWindow.isOpen()) {
-		//handleEvents();
-		if (Mouse::isButtonPressed(sf::Mouse::Right)) {
-			drawX();
+		sf::Event event;
+		while (gameWindow.pollEvent(event)) {
+			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+				render();
+				reviewBoard();
+				findCoordinates();
+			}
 		}
 
 	}
--- TicTacToe/TicTacToe/game.h
+++ TicTacToe/TicTacToe/game.h
@@ -2,16 +2,39 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 using namespace sf;
+const static int ROWS = 3;
+const static int COLUMNS = 3;
 class Game {
 private:
 	RenderWindow gameWindow;
 	Texture gameTexture;
 	Sprite gameBackground;
+	int playerNumber;
+	bool paused;
+	int matrix[ROWS][COLUMNS];
+	Vector2i board[ROWS][COLUMNS];
 public:
 	Game();
+	void fillBoard();
 	void render();
 	void openGame();
-	void drawX();
+	void changeShift();
+	void findPlayerNumber(float coordX, float coordY);
+	void findPosition(int row,int column);
+	void insertTopRight(Vector2i& mousePosition);
+	void insertTopCentral(Vector2i& mousePosition);
+	void insertTopLeft(Vector2i& mousePosition);
+	void insertCenterRight(Vector2i& mousePosition);
+	void insertCenter(Vector2i& mousePosition);
+	void insertCenterLeft(Vector2i& mousePosition);
+	void insertDownRight(Vector2i& mousePosition);
+	void insertDownCentral(Vector2i& mousePosition);
+	void insertDownLeft(Vector2i& mousePosition);
+	void findCoordinates();
+	void reviewBoard();
+	void refreshBoard(int numPlayer, int row,int column);
+	void drawCircle(float coordX, float coordY);
+	void drawX(float coordX, float coordY);
 	void showWindow();
 	void play();
 };
